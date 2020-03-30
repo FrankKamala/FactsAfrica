6d4a081f6f4754786b08afc54715f792279ed074
FrankKamala <frankkamala@gmail.com>
2020-03-30 10:34:02 +0300

    add splash screen delay

--- buyer/src/main/java/com/example/factsafrica/ui/SplashActivity.java
+++ buyer/src/main/java/com/example/factsafrica/ui/SplashActivity.java
@@ -2,7 +2,9 @@ package com.example.factsafrica.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.factsafrica.R;
 
@@ -12,5 +14,18 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class)); //starts main Activity
+                finish();
+
+            }
+        },  3000);
     }
+
+
+
 }
