5ba2f2840b9449b9e53d23e00cd2a33fcd8d6726
FrankKamala <frankkamala@gmail.com>
2020-03-30 12:51:18 +0300

    add mVendor constraint and animator

--- vendor/src/main/java/com/example/vendor/Splash.java
+++ vendor/src/main/java/com/example/vendor/Splash.java
@@ -1,10 +1,14 @@
 package com.example.vendor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 
 public class Splash extends AppCompatActivity {
+    ConstraintLayout myVendorConstraint;
+    AnimationDrawable myVendorAnimator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
