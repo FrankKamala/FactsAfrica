9c027142f416dc975bed1e5db4e05af2fb7698c9
FrankKamala <frankkamala@gmail.com>
2020-03-30 12:56:42 +0300

    add splash logic

--- vendor/src/main/java/com/example/vendor/Splash.java
+++ vendor/src/main/java/com/example/vendor/Splash.java
@@ -3,8 +3,12 @@ package com.example.vendor;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.vendor.ui.LoginActivity;
 
 public class Splash extends AppCompatActivity {
     ConstraintLayout myVendorConstraint;
@@ -14,5 +18,19 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        myVendorConstraint=(ConstraintLayout)findViewById(R.id.vendorSplash);
+        myVendorAnimator=(AnimationDrawable)myVendorConstraint.getBackground();
+        myVendorAnimator.setEnterFadeDuration(2000);
+        myVendorAnimator.setExitFadeDuration(2000);
+        myVendorAnimator.start();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash.this, LoginActivity.class)); //starts main Activity
+                finish();
+
+            }
+        },  3000);
     }
 }
